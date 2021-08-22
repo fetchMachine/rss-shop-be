85dae393c2c3a30ce2f0a46918d73c6ef8a6974f
Roman Makretki <r.makretski@gmail.com>
2021-08-22 18:27:25 +0300

    add: added getProducts tests

---
+++ product-service/src/functions/getProducts/getProducts.test.ts
@@ -0,0 +1,19 @@
+import type { APIGatewayProxyResult } from 'aws-lambda';
+
+import { getProducts } from './getProducts';
+
+import products from './products.mock.json';
+
+describe('getProducts', () => {
+  it('getProducts return list of all products', async () => {
+    const result = await getProducts();
+
+    const expectedResult: APIGatewayProxyResult = {
+      statusCode: 200,
+      body: JSON.stringify({ items: products }),
+    };
+
+
+    expect(result).toEqual(expectedResult);
+  });
+});
