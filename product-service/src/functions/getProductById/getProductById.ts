947b7b61a5a9de8fb614d87f09060ae7fddbe7f2
Roman Makretki <r.makretski@gmail.com>
2021-08-29 23:36:20 +0300

    add: added error handling

--- product-service/src/functions/getProductById/getProductById.ts
+++ product-service/src/functions/getProductById/getProductById.ts
@@ -7,25 +7,30 @@ import products from '@functions/products.mock.json';
 import { ERROS } from './errorMsgs';
 
 export const getProductById = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const { productId = '' } = event.pathParameters;
-  const productIdStringed = productId?.toString();
+  try {
+    const { productId = '' } = event.pathParameters;
+    const productIdStringed = productId?.toString();
 
-  if (!productIdStringed) {
-    return { statusCode: 400, body: JSON.stringify({ message: ERROS.INVALID_ID }) };
+    if (!productIdStringed) {
+      return { statusCode: 400, body: JSON.stringify({ message: ERROS.INVALID_ID }) };
+    }
+
+    const product = products.find((product) => product.id === productIdStringed);
+
+    if (!product) {
+      return { statusCode: 404, body: JSON.stringify({ message: ERROS.PRODUCT_NOT_FOUNDED }) };
+    }
+
+
+    return {
+      statusCode: 200,
+      headers: { ...COMMON_HEADERS },
+      body: JSON.stringify({
+        items: [product],
+      }),
+    };
+  } catch (e) {
+    console.log(e);
+    return { statusCode: 500, body: JSON.stringify({ message: 'something go wrong' }) }
   }
-
-  const product = products.find((product) => product.id === productIdStringed);
-
-  if (!product) {
-    return { statusCode: 404, body: JSON.stringify({ message: ERROS.PRODUCT_NOT_FOUNDED }) };
-  }
-
-
-  return {
-    statusCode: 200,
-    headers: { ...COMMON_HEADERS },
-    body: JSON.stringify({
-      items: [product],
-    }),
-  };
 }
--- product-service/src/functions/getProducts/getProducts.ts
+++ product-service/src/functions/getProducts/getProducts.ts
@@ -6,11 +6,16 @@ import { COMMON_HEADERS } from '@functions/constants';
 import products from '@functions/products.mock.json';
 
 export const getProducts = async (): Promise<APIGatewayProxyResult> => {
-  return {
-    statusCode: 200,
-    headers: { ...COMMON_HEADERS },
-    body: JSON.stringify({
-      items: products,
-    }),
-  };
+  try {
+    return {
+      statusCode: 200,
+      headers: { ...COMMON_HEADERS },
+      body: JSON.stringify({
+        items: products,
+      }),
+    };
+  } catch(e) {
+    console.log(e);
+    return { statusCode: 500, body: JSON.stringify({ message: 'something go wrong' }) }
+  }
 }
