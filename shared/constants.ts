b3679102080564959446304e7206ccbaa3d18263
r.makretski <r.makretski@andersenlab.com>
2021-11-06 18:20:50 +0300

    fix: fixed constants

--- shared/constants.ts
+++ shared/constants.ts
@@ -1,6 +1,6 @@
 export const COMMON_HEADERS = {
   "Access-Control-Allow-Headers" : "Content-Type",
-  "Access-Control-Allow-Origin": "https://dzfkk71q2g2nw.cloudfront.net",
+  "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "OPTIONS,GET"
 }
 
